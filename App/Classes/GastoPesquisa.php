e4d9e568af7a99f8f5bb4fb72ac8a2ea2e1a3e76
Diogo Schultz <diogorschultz2@gmail.com>
2025-07-01 11:15:17 -0300

    adicionando grafico, e comecando a usar os principios SOLID

---
+++ App/Classes/GastoPesquisa.php
@@ -0,0 +1,117 @@
+<?php
+namespace App\Classes;
+
+use PDO;
+
+class GastoPesquisa {
+    private $id_gasto;
+    private $produto;
+    private $categoria_id;
+    private $nome_categoria; //vem da outra tabela
+    private $valor;
+    private $vencimento;
+    private $forma_pagamento_id;
+    private $nome_forma_pagamento;//vem da outra tabela
+    private $parcelas_pagas;
+    private $total_parcelas;
+    private $data_pagamento;
+
+    public function __construct($id_gasto, $produto, $categoria_id, $nome_categoria, $valor, $vencimento, $forma_pagamento_id, $nome_forma_pagamento, $parcelas_pagas, $total_parcelas, $data_pagamento) {
+        $this->id_gasto = $id_gasto;
+        $this->produto = $produto;
+        $this->categoria_id = $categoria_id;
+        $this->nome_categoria = $nome_categoria;
+        $this->valor = $valor;
+        $this->vencimento = $vencimento;
+        $this->forma_pagamento_id = $forma_pagamento_id;
+        $this->nome_forma_pagamento = $nome_forma_pagamento;
+        $this->parcelas_pagas = $parcelas_pagas;
+        $this->total_parcelas = $total_parcelas;
+        $this->data_pagamento = $data_pagamento;
+    }
+
+    public function getProduto() { return $this->produto; }
+    public function getNomeCategoria() { return $this->nome_categoria; }
+    public function getValor() { return $this->valor; }
+    public function getVencimento() { return $this->vencimento; }
+    public function getNomeFormaPagamento() { return $this->nome_forma_pagamento; }
+    public function getParcelasPagas() { return $this->parcelas_pagas; }
+    public function getTotalParcelas() { return $this->total_parcelas; }
+    public function getDataPagamento() { return $this->data_pagamento; }
+
+
+    public static function getGasto(PDO $pdo) { //passando um objeto do tipo PDO, dizendo a conexao
+        $sql = "SELECT 
+                    a.*, 
+                    b.nome_categoria_gasto, 
+                    c.nome_forma_pagamento
+                FROM gasto a
+                JOIN categoria_gasto b ON a.categoria_id = b.id_categoria_gasto
+                JOIN forma_pagamento c ON a.forma_pagamento_id = c.id_forma_pagamento
+                ORDER BY a.vencimento DESC;
+                ";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $gastos = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {// PDO::FETCH_ASSOC é uma constante da classe PDO que representa o modo de retorno "array associativo"
+            $gastos[] = new Gasto(
+                $row['id_gasto'],
+                $row['produto'],
+                $row['categoria_id'],
+                $row['nome_categoria_gasto'],
+                $row['valor'],
+                $row['vencimento'],
+                $row['forma_pagamento_id'],
+                $row['nome_forma_pagamento'],
+                $row['parcelas_pagas'],
+                $row['total_parcelas'],
+                $row['data_pagamento']
+            );
+        }
+        return $gastos;
+    }
+
+    public function insertGasto(PDO $pdo){
+        $sql = "INSERT INTO gasto 
+           (produto, categoria_id, valor, vencimento, forma_pagamento_id, parcelas_pagas, total_parcelas, data_pagamento)
+           VALUES 
+           (:produto, :categoria_id, :valor, :vencimento, :forma_pagamento_id, :parcelas_pagas, :total_parcelas, :data_pagamento)";
+    
+        $stmt = $pdo->prepare($sql);
+        $stmt->bindParam(':produto', $this->produto);
+        $stmt->bindParam(':categoria_id', $this->categoria_id, PDO::PARAM_INT);
+        $stmt->bindParam(':valor', $this->valor);
+        $stmt->bindParam(':vencimento', $this->vencimento);
+        $stmt->bindParam(':forma_pagamento_id', $this->forma_pagamento_id, PDO::PARAM_INT);
+        $stmt->bindParam(':parcelas_pagas', $this->parcelas_pagas, PDO::PARAM_INT);
+        $stmt->bindParam(':total_parcelas', $this->total_parcelas, PDO::PARAM_INT);
+        $stmt->bindParam(':data_pagamento', $this->data_pagamento);
+
+        return $stmt->execute();
+    }
+
+    public static function getGastoAgrupadoPorMesAno(PDO $pdo){
+        $sql = "SELECT 
+                    DATE_FORMAT(a.data_pagamento, '%Y-%m') AS mes_ano,
+                    DATE_FORMAT(a.data_pagamento, '%M %Y') AS mes_ano_label,
+                    SUM(a.valor) AS total_valor
+                FROM gasto a
+                GROUP BY mes_ano
+                ORDER BY mes_ano DESC";
+
+        $stmt = $pdo->prepare($sql);
+        $stmt->execute();
+
+        $resultado = [];
+        while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
+            $resultado[] = [
+                'mes_ano' => $row['mes_ano'],
+                'mes_ano_label' => $row['mes_ano_label'],
+                'total_valor' => $row['total_valor']
+            ];
+        }
+        return $resultado;
+    }
+}
--- relatorio_gasto.php
+++ relatorio_gasto.php
@@ -3,11 +3,16 @@
 
     use App\Classes\Conexao;
     use App\Classes\Gasto;
+    use App\Classes\GastoPesquisa;
 
     $conexao = new Conexao();
     $pdo = $conexao->getPdo();
 
-    $gastoArray = Gasto::getGasto($pdo)
+    $gastoArray = Gasto::getGasto($pdo);
+
+    $GastoporMes = GastoPesquisa::getGastoAgrupadoPorMesAno($pdo);
+    $labels = array_column($GastoporMes, 'mes_ano_label');
+    $valores = array_column($GastoporMes, 'total_valor');
 ?>
 
 <!DOCTYPE html>
@@ -65,6 +70,41 @@
                     </tbody>
                 </table>
             </div>
+            <div class="content">
+                <canvas id="grafico1"></canvas>
+            </div>
         </main>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
+        <script>
+            const labels = <?php echo json_encode($labels); ?>;
+            const data = <?php echo json_encode($valores); ?>;
+            const ctx = document.getElementById('grafico1').getContext('2d');
+            const grafico = new Chart(ctx, {
+                type: 'bar',
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: 'Total de Gastos por Mês',
+                        data: data,
+                        borderWidth: 1,
+                        borderColor: '#6949ca',
+                        backgroundColor: '#6949ca',
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true,
+                            ticks: {
+                                callback: function(value) {
+                                    return 'R$ ' + value.toFixed(2).replace('.', ',');
+                                }
+                            }
+                        }
+                    }
+                }
+            });
+        </script>
     </body>
 </html>
